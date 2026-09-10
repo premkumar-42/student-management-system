8e2b40b8effe3273242421fa0bad89a1ca17943f
Prem Kumar <premkumar161142@gmail.com>
2026-09-10 13:49:16 +0530

    Create production superuser during deployment

--- build.sh
+++ build.sh
@@ -4,3 +4,5 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py migrate
 python manage.py collectstatic --no-input
+
+python manage.py shell -c "from django.contrib.auth import get_user_model; User=get_user_model(); User.objects.filter(username='$DJANGO_SUPERUSER_USERNAME').exists() or User.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME', '$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD')"
